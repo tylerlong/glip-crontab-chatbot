8419ff3ad2b6f07707ead610f66f59fed7860515
Tyler Long <tyler4long@gmail.com>
2018-11-17 20:06:19 +0800

    Upgrade ringcentral-chatbot

--- src/express.js
+++ src/express.js
@@ -1,10 +1,9 @@
-import app from 'ringcentral-chatbot/dist/apps/index'
+import createApp from 'ringcentral-chatbot/dist/apps'
 
 import { handle } from './eventHandler'
 import crontab from './crontab'
 
+const app = createApp(handle)
 app.listen(process.env.RINGCENTRAL_CHATBOT_EXPRESS_PORT)
-
-app.$.subscribe(event => handle(event))
 
 setInterval(() => crontab(), 60000)
