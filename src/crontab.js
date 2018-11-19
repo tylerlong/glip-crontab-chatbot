9c9cf5de18e5c627281466597bc7ea543db3ff0b
Tyler Long <tyler4long@gmail.com>
2018-11-19 15:59:37 +0800

    Catch cron job exception

--- src/crontab.js
+++ src/crontab.js
@@ -10,7 +10,11 @@ const crontab = async () => {
     const prevTimestamp = interval.prev()._date
     if (currentTimestamp - prevTimestamp === 0) {
       const bot = await Bot.findByPk(service.botId)
-      await bot.sendMessage(service.groupId, { text: service.data.message })
+      try {
+        await bot.sendMessage(service.groupId, { text: service.data.message })
+      } catch (e) { // catch the exception so that it won't break the for loop
+        console.error(e)
+      }
     }
   }
 }
