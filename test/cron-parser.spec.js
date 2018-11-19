b7c8bf38228d91ccee57d9a1c07dd0ad4f772218
Tyler Long <tyler4long@gmail.com>
2018-11-19 10:13:43 +0800

    Add cron test case about timezone

--- test/cron-parser.spec.js
+++ test/cron-parser.spec.js
@@ -3,7 +3,7 @@ import cronParser from 'cron-parser'
 import moment from 'moment-timezone'
 
 describe('cron-parser', () => {
-  test('default', async () => {
+  test('UTC', async () => {
     const interval = cronParser.parseExpression('* * * * *', { utc: true })
     const prevTimestamp = interval.prev()._date
     const currentTimestamp = moment.tz(new Date(), 'utc').seconds(0).milliseconds(0)
@@ -16,5 +16,12 @@ describe('cron-parser', () => {
 
   test('syntax must have 5 tokens', () => {
     expect('*/2 * * * *'.split(/\s+/).length).toBe(5)
+  })
+
+  test('timezone', () => {
+    const interval = cronParser.parseExpression('* * * * *', { tz: 'Asia/Shanghai' })
+    const prevTimestamp = interval.prev()._date
+    const currentTimestamp = moment.tz(new Date(), 'Asia/Shanghai').seconds(0).milliseconds(0)
+    expect(currentTimestamp - prevTimestamp).toBe(0)
   })
 })
